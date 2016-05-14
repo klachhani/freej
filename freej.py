e605785871252ca4108bb33cab4d99ea57af3d01
Kishan Lachhani <kishan.lachhani@gmail.com>
2016-05-14 19:32:26 +0100

    added user page

--- freej.py
+++ freej.py
@@ -5,19 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'You are home!'
+    return 'Hello World!'
 
+@app.route('/user/<userid>')
+def load_user_page(userid):
+    return 'Hey, youre userid is %s' % userid
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'The user is...%s' % username
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
