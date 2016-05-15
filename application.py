a318cf64f2232aa3e2a6b877c15cf1502c087f79
Kishan Lachhani <kishan.lachhani@gmail.com>
2016-05-15 06:17:00 +0100

    route ready for data

--- application.py
+++ application.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 import json
 import os
+import bigoven.recipes as recipes
 
 application = Flask(__name__)
 application.debug = True
 
-
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
 
 @application.route('/')
 def home():
     return render_template('hello.html')
 
+
 @application.route('/user/<userid>')
 def load_user_page(userid):
     json_url = os.path.join(SITE_ROOT, "static/db", userid + ".json")
-    with open(json_url, 'r') as data_file:
-        data = json.load(data_file)
-        ingredients = data['Ingredients']
+    data = json.load(open(json_url, 'r'))
+    ingredients = data['Ingredients']
+    recipe_ids = recipes.search_recipes(ingredients)
+    recipe_list = []
+    for i in recipe_ids[:20]:
+        recipe_list.append(recipes.get_recipe(i))
 
-
-
-    return render_template('user.html', ing=ingredients)
+    return render_template('user.html', ing=ingredients, recipes=recipe_list)
 
 
 
--- bigoven/recipes.py
+++ bigoven/recipes.py
@@ -1,16 +1,39 @@
 import requests
 import itertools
+import threading
 
 
 def search_recipes(ingredients):
-    combs_final = []
     maxSubset = min(3, len(ingredients))
     combs = list(itertools.combinations(ingredients, maxSubset))
+    delim = '%2C'
+    recipe_ids = []
 
-    #for c in combs:
 
-    #    url = 'https://api2.bigoven.com/recipes?include_ing=pasta%2C%20tomatoes%2C%20apples%2C%20vinegar%2C%20asparagus%2C%20augbergine&api_key=jA6APRL318vmXZf4GxUn6059YRwvWZ6W%''
-    return combs
+    def worker(url):
+        data = requests.get(url)
+        if data.ok:
+            data = data.json()
+            recipes = data['Results']
+            for r in recipes:
+                recipe_ids.append(r['RecipeID'])
+
+
+    threads = []
+    for c in combs:
+        query = ''
+        for i in c:
+            query += i + delim
+        query = query[:-3]
+        url = 'https://api2.bigoven.com/recipes?include_ing=' + query + '&api_key=jA6APRL318vmXZf4GxUn6059YRwvWZ6W'
+        t = threading.Thread(target=worker, args=(url,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return recipe_ids
 
 
 def get_recipe(recipe_id):
@@ -25,15 +48,8 @@ def get_recipe(recipe_id):
         maxImageSq = str(data['MaxImageSquare'])
         recipe['ImageURL'] = data['ImageURL'] + '?h=' + maxImageSq + '&w=' + maxImageSq
         recipe['Description'] = data['Description']
-
-
+        recipe['YieldNumber'] = data['YieldNumber']
+        recipe['Servings'] = data['Servings']
+        recipe['Instructions'] = data['Instructions']
 
     return recipe
-
-
-c = search_recipes(['pears', 'tomato', 'pasta', 'cheese', 'milk'])
-delim = '%2C'
-q = ''
-for i in c[0]:
-    q += i + delim
-print q[:]
